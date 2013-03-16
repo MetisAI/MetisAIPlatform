616bd52cc728b8c052e9f05abfd5c5c3bd54b99b
Kwc-Oliver <kwc.oliver@gmail.com>
2013-03-16 21:43:15 +0800

    去掉无用文件...

--- htdocs/2.11/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$username='root';
-	$userpass='dragoncat';
-	$dbhost='localhost';
-	$dbdatabase='table2';
-	$db_connect=mysql_connect($dbhost,$username,$userpass) or die("Unable to connect to the MySQL!");;
-	mysql_select_db($dbdatabase,$db_connect);
-	
-?>
--- htdocs/2.7/check_user_exist.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require("dbconnect.php");
-	$result=mysql_query("SELECT CHECK_USERNAME('".$_POST["user"]."')");
-	$ans=mysql_fetch_array($result);
-	if ($ans[0]=='N') echo "NO";
-	else echo "YES";
-	mysql_close();
-?>
